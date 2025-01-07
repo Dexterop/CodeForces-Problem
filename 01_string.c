0eef4affa2614e0e39e316966d79894e8f91aa67
Hardik Yadav <75905746+Dexterop@users.noreply.github.com>
2025-01-07 09:40:45 +0530

    Add files via upload
    question - https://codeforces.com/problemset/problem/71/A

---
+++ 01_string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    // long word = more than 10 char
+    int n;
+    scanf("%d",&n);
+
+    for (int i = 0; i < n; i++){
+        char word[101];
+        scanf("%s",&word);
+
+        int length = strlen(word);
+        if (length > 10)
+        {
+            printf("%c%d%c\n", word[0], length - 2 ,word[length - 1]);
+        }
+        else{
+            printf("%s\n", word);
+        }
+    }
+    
+
+    return 0;
+}
